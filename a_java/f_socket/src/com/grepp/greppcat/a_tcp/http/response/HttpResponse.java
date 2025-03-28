e9cda0e3ba7eab8a29267b9502c2f77748fa33dc
chanwoo kim <dlfeh741852@naver.com>
2025-03-28 10:27:27 +0900

    thread

---
+++ a_java/f_socket/src/com/grepp/greppcat/a_tcp/http/response/HttpResponse.java
@@ -0,0 +1,11 @@
+package com.grepp.greppcat.a_tcp.http.response;
+
+import com.grepp.greppcat.a_tcp.http.HttpHeader;
+
+public record HttpResponse(
+    ResponseStartLine startLine,
+    HttpHeader httpHeader,
+    ResponseBody body
+) {
+
+}
