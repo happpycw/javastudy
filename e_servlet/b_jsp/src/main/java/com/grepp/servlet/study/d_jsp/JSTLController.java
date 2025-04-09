6d5dfb827052bebb603ac239482d7ec9767ceae3
dlfeh741852@naver.com <dlfeh741852@naver.com>
2025-04-09 12:07:08 +0900

    mm

---
+++ e_servlet/b_jsp/src/main/java/com/grepp/servlet/study/d_jsp/JSTLController.java
@@ -0,0 +1,23 @@
+package com.grepp.servlet.study.d_jsp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet("/jstl/page")
+public class JSTLController extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        String xss = "<script>alert('xss attack!');</script>";
+        req.setAttribute("xss", xss);
+        req.setAttribute("now", LocalDateTime.now());
+        req.getRequestDispatcher("/WEB-INF/view/jstl.jsp")
+            .forward(req, resp);
+    }
+}
