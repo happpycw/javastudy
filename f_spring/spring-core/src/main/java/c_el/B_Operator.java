6665dd248ae1a8de5d7ab2e88584c72ebe1abb11
dlfeh741852@naver.com <dlfeh741852@naver.com>
2025-04-11 22:05:32 +0900

    day 29

--- f_spring/spring-core/src/main/java/c_el/B_Operator.java
+++ f_spring/spring-core/src/main/java/c_el/B_Operator.java
@@ -6,33 +6,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class B_Operator {
 
-    @Value("#{10 + 20}")
+    @Value("#{ 10 + 20}")
     private int add;
-
-    @Value("#{10 / 20}")
+    
+    @Value("#{ 10 / 20}")
     private int divide;
-
-    @Value("#{10 == 20}")
+    
+    @Value("#{10 == 10}")
     private boolean equal;
-
-    @Value("#{10 > 20}")
+    
+    @Value("#{100 > 20}")
     private boolean greater;
-
+    
     @Value("#{true or false}")
     private boolean or;
-
+    
     @Value("#{!true}")
     private boolean not;
-
+    
     @Override
     public String toString() {
         return "B_Operator{" +
-            "add=" + add +
-            ", divide=" + divide +
-            ", equal=" + equal +
-            ", greater=" + greater +
-            ", or=" + or +
-            ", not=" + not +
-            '}';
+                   "add=" + add +
+                   ", divide=" + divide +
+                   ", equal=" + equal +
+                   ", greater=" + greater +
+                   ", or=" + or +
+                   ", not=" + not +
+                   '}';
     }
 }
--- f_spring/spring-core/src/main/java/c_el/bean/TransactionManager.java
+++ f_spring/spring-core/src/main/java/c_el/bean/TransactionManager.java
@@ -16,15 +16,19 @@ public class TransactionManager {
         this.jdbcProperties = jdbcProperties;
         this.jdbcTemplate = jdbcTemplate;
     }
-
+    
+    public String connect(){
+        return "DB 연결에 성공했습니다. 트랜잭션을 시작합니다.";
+    }
+    
     public JdbcProperties getJdbcProperties() {
         return jdbcProperties;
     }
-
+    
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
     }
-
+    
     @Override
     public String toString() {
         return "TransactionManager{" +
