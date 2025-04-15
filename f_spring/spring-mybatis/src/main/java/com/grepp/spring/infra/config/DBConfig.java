9fbd481478444a6a360fe888d7e759e854608586
dlfeh741852@naver.com <dlfeh741852@naver.com>
2025-04-15 18:17:23 +0900

    day 32

--- f_spring/spring-mybatis/src/main/java/com/grepp/spring/infra/config/DBConfig.java
+++ f_spring/spring-mybatis/src/main/java/com/grepp/spring/infra/config/DBConfig.java
@@ -13,33 +13,35 @@ import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class DBConfig {
-
+    
     @Value("${spring.datasource.url}")
     private String url;
-
+    
     @Value("${spring.datasource.username}")
     private String username;
-
+    
     @Value("${spring.datasource.password}")
     private String password;
-
+    
     @Value("${spring.datasource.driver}")
     private String driver;
-
+    
     @Value("${spring.datasource.hikari.maximum-pool-size}")
     private int poolSize;
-
+    
     @Value("${schema.script.name:empty-schema.sql}")
     private String schema;
-
+    
     @Value("${data.script.name:empty-data.sql}")
     private String data;
-
+    
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(url);
         config.setDriverClassName(driver);
@@ -48,37 +50,36 @@ public class DBConfig {
         config.setMaximumPoolSize(poolSize);
         return new HikariDataSource(config);
     }
-
+    
     @PreDestroy
-    public void cleanup(){
+    public void cleanup() {
         AbandonedConnectionCleanupThread.checkedShutdown();
     }
-
+    
     @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource){
-        return new DataSourceTransactionManager(dataSource);
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
-
+    
     @Bean
-    public ResourceDatabasePopulator databasePopulator(){
+    public ResourceDatabasePopulator databasePopulator() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        Resource schemaResource = new ClassPathResource("init/"  + schema);
+        Resource schemaResource = new ClassPathResource("init/" + schema);
         Resource dataResource = new ClassPathResource("init/" + data);
-
+        
         populator.addScripts(schemaResource, dataResource);
         populator.setContinueOnError(false);
         return populator;
     }
-
+    
     @Bean
     public DataSourceInitializer databasePopulatorInitializer(
-        DataSource dataSource, ResourceDatabasePopulator databasePopulator){
-
+        DataSource dataSource, ResourceDatabasePopulator databasePopulator) {
         DataSourceInitializer initializer = new DataSourceInitializer();
         initializer.setDataSource(dataSource);
         initializer.setDatabasePopulator(databasePopulator);
         return initializer;
-
     }
-
+    
+    
 }
--- f_spring/spring-mybatis/src/main/resources/init/data.sql
+++ f_spring/spring-mybatis/src/main/resources/init/data.sql
@@ -1,4 +1,3 @@
-
 insert into book(bk_idx, author, amount, category, title, created_at)
 values (1000, '김애란', '3', 'NOVEL', '비행운', '2023-03-06 3:02:40');
 insert into book(bk_idx, author, amount, category, title, created_at)
@@ -9,6 +8,46 @@ insert into book(bk_idx, author, amount, category, title, created_at)
 values (1003, '비트겐슈타인', '6', 'HUMANITY', '논리철학논고', '2025-04-06 3:02:40');
 insert into book(bk_idx, author, amount, category, title, created_at)
 values (1004, '황정은', '7', 'NOVEL', '디디의우산', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('김애란', '3', 'NOVEL', '비행운', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('조앤롤링', '4', 'NOVEL', '해리포터', '2023-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('에리히프롬', '5', 'HUMANITY', '사랑의기술', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('비트겐슈타인', '6', 'HUMANITY', '논리철학논고', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('황정은', '7', 'NOVEL', '디디의우산', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('김애란', '3', 'NOVEL', '비행운', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('조앤롤링', '4', 'NOVEL', '해리포터', '2023-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('에리히프롬', '5', 'HUMANITY', '사랑의기술', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('비트겐슈타인', '6', 'HUMANITY', '논리철학논고', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('황정은', '7', 'NOVEL', '디디의우산', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('김애란', '3', 'NOVEL', '비행운', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('조앤롤링', '4', 'NOVEL', '해리포터', '2023-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('에리히프롬', '5', 'HUMANITY', '사랑의기술', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('비트겐슈타인', '6', 'HUMANITY', '논리철학논고', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('황정은', '7', 'NOVEL', '디디의우산', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('김애란', '3', 'NOVEL', '비행운', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('조앤롤링', '4', 'NOVEL', '해리포터', '2023-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('에리히프롬', '5', 'HUMANITY', '사랑의기술', '2023-03-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('비트겐슈타인', '6', 'HUMANITY', '논리철학논고', '2025-04-06 3:02:40');
+insert into book(author, amount, category, title, created_at)
+values ('황정은', '7', 'NOVEL', '디디의우산', '2025-04-06 3:02:40');
 
 insert into member(user_id, email, role, password, tel, created_at)
 values ('jpa', 'jpa@jpa.com', 'ROLE_USER', '1234', '010-0112-0119', '2023-03-06 3:02:40');
@@ -40,7 +79,7 @@ values (200001, 100001, 1000, '비행운', 'RENT', '2023-03-13 3:02:40', '2023-0
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
 values (200002, 100001, 1001, '해리포터', 'RENT', '2023-04-13 3:02:40', '2023-04-06 3:02:40');
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
-values (200003, 100002, 1000, '비행운','RENT', '2023-03-13 3:02:40', '2023-03-06 3:02:40');
+values (200003, 100002, 1000, '비행운', 'RENT', '2023-03-13 3:02:40', '2023-03-06 3:02:40');
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
 values (200004, 100002, 1001, '해리포터', 'RENT', '2025-04-13 3:02:40', '2025-03-06 3:02:40');
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
@@ -50,7 +89,7 @@ values (200006, 100003, 1001, '해리포터', 'RENT', '2023-03-13 3:02:40', '202
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
 values (200007, 100004, 1004, '디디의우산', 'RENT', '2023-04-13 3:02:40', '2023-04-06 3:02:40');
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
-values (200008, 100004, 1003, '논리철학논고','RENT', '2023-03-13 3:02:40', '2023-03-06 3:02:40');
+values (200008, 100004, 1003, '논리철학논고', 'RENT', '2023-03-13 3:02:40', '2023-03-06 3:02:40');
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
 values (200009, 100005, 1004, '디디의우산', 'RENT', '2025-04-13 3:02:40', '2025-03-06 3:02:40');
 insert into rent_book(rb_idx, rent_id, bk_idx, book_title, state, return_date, created_at)
